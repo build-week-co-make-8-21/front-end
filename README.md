69e868b002a0fbbe184f743fbfae043fd3c4b13c
ianbautista <ianfbautista@gmail.com>
2020-08-22 04:58:31 -0700

    initial project setup.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# front-end
+# Co-Make 👨‍👩‍👧‍👦 Build Week Project
+
+### August 2020
+
+## Front-End Developers:
+
+| Junior React  | Senior React       |
+| ------------- | ------------------ |
+| Mike Mitchell | Christian Bautista |
+| Trevor Armes  |
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./Assets/co-make-logo-v2.png";
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>Co-make is your local hub to resolve issues in your community.</p>
+			</header>
+		</div>
+	);
+}
+
+export default App;
